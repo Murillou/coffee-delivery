a5433e378b7b2dd1800c9b3ab85994896533b550
Murillou <Murillovinicius13@hotmail.com>
2024-05-12 02:43:43 -0300

    [feat] API de geolocalização e geocodificação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import coffeelogo from '../assets/logo-coffee.png';
+import { useState, useEffect } from 'react';
 
 export function Header() {
+  const [city, setCity] = useState('');
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(async position => {
+        const { latitude, longitude } = position.coords;
+        try {
+          const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyBre0xp45CXu5ycVVZdNEL4TZAIhE4PhTE`
+          );
+
+          const data = await response.json();
+          const cityAndState = data.results
+            .map((result: any) => {
+              const cityComponent = data.results[7]?.address_components.find(
+                (component: any) => component.types.includes('locality')
+              )?.long_name;
+
+              const stateComponent = result.address_components.find(
+                (component: any) =>
+                  component.types.includes('administrative_area_level_1')
+              )?.short_name;
+
+              return `${cityComponent}, ${stateComponent}`;
+            })
+            .find((city: string) => city);
+          setCity(cityAndState || 'Cidade desconhecida');
+        } catch (error) {
+          console.log('Erro ao obter o nome da cidade', error);
+          setCity('Cidade Desconhecida');
+        }
+      });
+    } else {
+      alert('Não foi possível pegar sua localização');
+    }
+  }, []);
+
   return (
     <header>
       <div className="flex justify-between items-center h-[6.5rem] p-4 mx-40">
@@ -12,7 +51,7 @@ export function Header() {
         <div className="flex gap-3">
           <div className="flex gap-1 items-center p-2 bg-purple-light text-purple-dark rounded-md cursor-pointer hover:bg-purple-normal hover:text-purple-light">
             <MapPin size={22} weight="fill" />
-            <span>João Pessoa, PB</span>
+            <span>{city}</span>
           </div>
 
           <ShoppingCart
