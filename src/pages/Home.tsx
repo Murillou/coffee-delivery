f8227c61585f6bacda8a2278b0c3e3d73905df24
Murillou <Murillovinicius13@hotmail.com>
2024-05-16 19:05:54 -0300

    [fix] retirando header duplicado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import { Header } from '../components/Header/Header';
 import { Main } from '../components/Main/Main';
 import { Menu } from '../components/Menu/Menu';
 
 export function Home() {
   return (
     <>
-      <Header />
       <Main />
       <Menu />
     </>
